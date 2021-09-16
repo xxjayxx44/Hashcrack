1eba6ac847a77670ebb988fe238dcb02c70145a9
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2021-09-16 16:35:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,94 @@
+# DecodeX
+
+```Created By: MrHacker-X```
+
+![photo](https://raw.githubusercontent.com/MrHacker-X/DecodeX/main/.img/banner.jpg)
+
+## Introduction
++ This is a password hashes bruteforcer which is written in python3. You can decrypt 6 type of hashes algorithms hashes that is mentioned down below:
++ ```MD5```
++ ```SHA1```
++ ```SHA224```
++ ```SHA256```
++ ```SHA384```
++ ```SHA512```
+
+## Used python module
++ hashlib
+
+## Tested ON
+
++ Termux (android)
++ Kali linux
++ Debian
++ Ubuntu
++ void
++ alpine
++ garuda
+
+## Available for
+
++ Termux (android)
++ Kali linux
++ Debian
++ Ubuntu
++ void
++ alpine
++ garuda
+
+## Requirements
+
++ Python3
+
+## Installation
++ Just Type and ENTER all the commands one by one in your terminal. If you don't want to type and ENTER all commands one by one then you can install this tool by only one line command that is also given down below
+
+```
+apt-get update -y
+apt-get upgrade -y
+apt-get install git python3 -y
+git clone https://github.com/MrHacker-X/DecodeX.git/
+cd DecodeX
+python3 decodex.py
+```
+
+## One line installation
+
+```
+apt-get update -y;apt-get upgrade -y;apt-get install git python3 -y;git clone https://github.com/MrHacker-X/DecodeX.git/;cd DecodeX;python3 decodex.py
+```
+
+
+## Screenshots
+
+#### Main menu
+![photo](https://raw.githubusercontent.com/MrHacker-X/DecodeX/main/.img/menu.jpg)
+#### Wordlist Input
+![photo](https://raw.githubusercontent.com/MrHacker-X/DecodeX/main/.img/pass.jpg)
+#### Getting ready for Crack
+![photo](https://raw.githubusercontent.com/MrHacker-X/DecodeX/main/.img/loading.jpg)
+#### Hash is cracked
+![photo](https://raw.githubusercontent.com/MrHacker-X/DecodeX/main/.img/brute.jpg)
+
+
+## Warning:
++ We are not responsible for any misuse or damage caused by this program. use this tool at your own risk!
+
+<h3><b><i>📡 Connect with us :</i></b></h3>
+<a href="https://github.com/MrHacker-X/"><img align="left" title="Github" alt="Github" width="30px" src="https://raw.githubusercontent.com/MrHacker-X/MrHacker-X/main/assets/github.png" /></a>
+<a href="https://instagram.com/hackerxmr/"><img align="left" title="Instagram" alt="Instagram" width="30px" src="https://raw.githubusercontent.com/MrHacker-X/MrHacker-X/main/assets/instagram.png" /></a>
+<a href="https://t.me/mrhackersx/"><img align="left" title="Telegram" alt="Telegram" width="30px" src="https://raw.githubusercontent.com/MrHacker-X/MrHacker-X/main/assets/telegram.png" /></a>
+<a href="https://youtube.com/c/Sololex/"><img align="left" title="YouTube" alt="YouTube" width="30px" src="https://raw.githubusercontent.com/MrHacker-X/MrHacker-X/main/assets/youtube.png" /></a>
+
+#
+<h3><b><i>🏆 Some popular Repository :</i></b></h3>
+
+<p align="center"><a href="https://github.com/MrHacker-X/SploitX.git/"><img title="SploitX" src="https://github-readme-stats.vercel.app/api/pin/?username=MrHacker-X&repo=SploitX&theme=radical"></a>
+<p align="center"><a href="https://github.com/MrHacker-X/CloneWeb.git/"><img title="CloneWeb" src="https://github-readme-stats.vercel.app/api/pin/?username=MrHacker-X&repo=CloneWeb&theme=radical"></a>
+<p align="center"><a href="https://github.com/MrHacker-X/Hacked.git/"><img title="Hacked" src="https://github-readme-stats.vercel.app/api/pin/?username=MrHacker-X&repo=CloneWeb&theme=radical"></a>
+<p align="center"><a href="https://github.com/MrHacker-X/Hack-OS.git/"><img title="Hack-OS" src="https://github-readme-stats.vercel.app/api/pin/?username=MrHacker-X&repo=CloneWeb&theme=radical"></a>
+<p align="center"><a href="https://github.com/MrHacker-X/termux-fingerprint.git/"><img title="termux-fingerprint" src="https://github-readme-stats.vercel.app/api/pin/?username=MrHacker-X&repo=termux-fingerprint&theme=radical"></a>
+
+
+
+
---
+++ decodex.py
@@ -0,0 +1,225 @@
+# /bin/bash/python !
+
+import os
+import time
+from time import sleep
+
+def load():
+
+	def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+		percent = ('{0:.' + str(decimals) + 'f}').format(100 * (iteration/float(total)))
+		filledLength = int(length * iteration // total)
+		bar = fill * filledLength + '-' * (length - filledLength)
+		print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
+		if iteration == total:
+			print()
+
+	items = list(range(0, 30))
+	l = len(items)
+
+	loadbar(0, l, prefix='Progress:', suffix='Complete', length=l)
+	for i, item in enumerate(items):
+		sleep(0.2)
+		loadbar(i + 1, l, prefix='Progress:', suffix='Complete', length=l)
+
+banner = ('''    ____                      __    _  __
+   / __ \___  _________  ____/ /__ | |/ /
+  / / / / _ \/ ___/ __ \/ __  / _ \|   /
+ / /_/ /  __/ /__/ /_/ / /_/ /  __/   |
+/_____/\___/\___/\____/\__,_/\___/_/|_|
+   ..:: Created by: MrHacker-X ::..
+''')
+
+main_menu = ('''[##] Select an option:
+
+[01] MD5
+[02] SHA1
+[03] SHA224
+[04] SHA256
+[05] SHA384
+[06] SHA512
+[07] about
+[99] Exit
+
+[~] alex: ''')
+
+pass_select = ('''\n[##] What kind of word list would you like to use:
+
+[01] Custom
+[02] Default
+[95] Back
+[99] Exit
+
+[~] alex: ''')
+
+os.system('clear')
+print(banner)
+menu = input(main_menu)
+
+if menu == '01' or menu == '1':
+	while True:
+		pas = input(pass_select)
+		if pas == '01' or pas == '1':
+			print()
+			os.system('python .alex/md5.py')
+			break
+		elif pas == '02' or pas == '2':
+			print()
+			os.system('python .alex/md5_auto.py')
+			break
+		elif pas == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif pas == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+		
+elif menu == '02' or menu == '2':
+	while True:
+		paas = input(pass_select)
+		if paas == '01' or paas == '1':
+			print()
+			os.system('python .alex/sha1.py')
+			break
+		elif paas == '02' or paas == '2':
+			print()
+			os.system('python .alex/sha1_auto.py')
+			break
+		elif paas == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif paas == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+
+elif menu == '03' or menu == '3':
+	while True:
+		pasa = input(pass_select)
+		if pasa == '01' or pasa == '1':
+			print()
+			os.system('python .alex/sha224.py')
+			break
+		elif pasa == '02' or pasa == '2':
+			print()
+			os.system('python .alex/sha224_auto.py')
+			break
+		elif pasa == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif pasa == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+
+elif menu == '04' or menu == '4':
+	while True:
+		pa = input(pass_select)
+		if pa == '01' or pa == '1':
+			print()
+			os.system('python .alex/sha256.py')
+			break
+		elif pa == '02' or pa == '2':
+			print()
+			os.system('python .alex/sha256_auto.py')
+			break
+		elif pa == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif pa == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+
+elif menu == '05' or menu == '5':
+	while True:
+		paa = input(pass_select)
+		if paa == '01' or paa == '1':
+			print()
+			os.system('python .alex/sha384.py')
+			break
+		elif paa == '02' or paa == '2':
+			print()
+			os.system('python .alex/sha384_auto.py')
+			break
+		elif paa == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif paa == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+
+elif menu == '06' or menu == '6':
+	while True:
+		pasp = input(pass_select)
+		if pasp == '01' or pasp == '1':
+			print()
+			os.system('python .alex/sha512.py')
+			break
+		elif pasp == '02' or pasp == '2':
+			print()
+			os.system('python .alex/sha512_auto.py')
+			break
+		elif pasp == '95':
+			print('\nComing back\n')
+			time.sleep(1)
+			os.system('python decodex.py')
+			break
+		elif pasp == '99':
+			print("\nExiting...\n")
+			time.sleep(1)
+			exit()
+			
+		else:
+			print('\nInvalid input\n')
+			time.sleep(1)
+
+
+elif menu == '07' or menu == '7':
+	print("\nWe're trying to do our best")
+	time.sleep(1)
+	os.system('python decodex.py')
+
+elif menu == "99":
+	print("\nExiting...\n")
+	time.sleep(1)
+	exit()
+
+else:
+	print('\nInvalid input\n')
+	time.sleep(1)
+	os.system('python decodex.py')
+
+
